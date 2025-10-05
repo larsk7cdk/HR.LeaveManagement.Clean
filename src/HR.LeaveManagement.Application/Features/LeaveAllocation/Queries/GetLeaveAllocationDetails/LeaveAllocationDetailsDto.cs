3f9253786988f5b251d36daa3e34c1bd21e13dd9
Lars <lars@k7c.dk>
2025-10-05 18:52:05 +0200

    Fixed dto

--- src/HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocationDetails/LeaveAllocationDetailsDto.cs
+++ src/HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocationDetails/LeaveAllocationDetailsDto.cs
@@ -1,10 +1,12 @@
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
 namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
 
 public class LeaveAllocationDetailsDto
 {
     public string EmployeeId { get; init; } = string.Empty;
     public int LeaveTypeId { get; init; }
-    public Domain.LeaveType? LeaveType { get; init; }
+    public LeaveTypeDto LeaveType { get; init; }
     public int NumberOfDays { get; init; }
     public int Period { get; init; }
     public DateTime? DateCreated { get; init; }
--- src/HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocations/LeaveAllocationDto.cs
+++ src/HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocations/LeaveAllocationDto.cs
@@ -1,10 +1,12 @@
-﻿namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations;
+﻿using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations;
 
 public class LeaveAllocationDto
 {
     public string EmployeeId { get; init; }
     public int LeaveTypeId { get; init; }
-    public Domain.LeaveType? LeaveType { get; init; }
+    public LeaveTypeDto LeaveType { get; init; }
     public int NumberOfDays { get; init; }
     public int Period { get; init; }
 }
